fdf9593578e72d71d4f3a8b7a16715960cba2452
Simon Praetorius <praetorius@sitegeist.de>
2020-04-10 12:57:41 +0200

    [TASK] Prepare for release of v2

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,15 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['sms_responsive_images'] = [
     'title' => 'SMS Responsive Images',
     'description' => 'Provides ViewHelpers and configuration to render valid responsive images based on TYPO3\'s image cropping tool.',
     'category' => 'fe',
     'author' => 'Simon Praetorius',
     'author_email' => 'praetorius@sitegeist.de',
     'author_company' => 'sitegeist media solutions GmbH',
-    'state' => 'beta',
+    'state' => 'stable',
     'uploadfolder' => false,
     'clearCacheOnLoad' => false,
-    'version' => '2.0.0-dev',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.99-10.9.99',
