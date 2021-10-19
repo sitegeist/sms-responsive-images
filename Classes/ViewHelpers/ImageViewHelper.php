ab667030a72193a361e1a69ae497f3a137e64a9c
Benjamin Tammling <benjamin.tammling@sitegeist.de>
2021-10-19 10:19:25 +0200

    [TASK] Add missing arguments to viewhelper for TYPO3 9.5

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -60,6 +60,18 @@ class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
             false,
             'svg, gif'
         );
+
+        $this->registerArgument(
+            'fileExtension',
+            'string',
+            'Custom file extension to use for images'
+        );
+
+        $this->registerArgument(
+            'loading',
+            'string',
+            'Native lazy-loading for images property. Can be "lazy", "eager" or "auto". Used on image files only.'
+        );
     }
 
     /**
